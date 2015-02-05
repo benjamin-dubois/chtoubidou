73104f346c5f81f22a24dd715f0f5716fbb22e24
Romain Lespinasse <romain.lespinasse@gmail.com>
2015-02-05 10:58:20 +0100

    Create chtoubidou.sh

---
+++ chtoubidou.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Le chtoubidou sait tout faire. Comme outil de test, c'est l'outil qu'il vous faut ! 
+# A la fois injecteur, simulateur, lecteur, relecteur, il simule n'importe quel de vos use case !
+# Plus besoin de développeurs, pensez à quelque chose et il vous le fait !
+# En plus c'est pas cher !
+
+echo "Voilà, c'est fait"
